e227aadd4916325ec2e3b5828417d581331e776f
Bibhuti Agarwal <developer.bibhuti@gmail.com>
2018-09-07 10:09:21 +0530

    initial version

---
+++ spring5entity/src/main/java/com/bsa/spring5entity/Spring5entityApplication.java
@@ -0,0 +1,12 @@
+package com.bsa.spring5entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5entityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5entityApplication.class, args);
+	}
+}
