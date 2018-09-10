6ffe9f3df070ad8609f7561178191cc94799b009
bibhuti-agarwal <bibhuti.agarwal2015@gmail.com>
2018-09-10 10:58:32 +0530

    initial version

---
+++ spring5entity/src/main/java/com/bsa/spring5entity/controller/AuthorController.java
@@ -0,0 +1,26 @@
+package com.bsa.spring5entity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.bsa.spring5entity.repositories.AuthorRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class AuthorController {
+	private AuthorRepository authorRepository;
+
+	public AuthorController(AuthorRepository authorRepository) {
+		super();
+		this.authorRepository = authorRepository;
+	}
+
+	@RequestMapping(value="/authors", method=RequestMethod.GET)
+	public String requestMethodName(Model model) {
+		model.addAttribute("", authorRepository.findAll()); 
+		return "authors";
+	}
+	
+}
---
+++ spring5entity/src/main/java/com/bsa/spring5entity/controller/PublisherController.java
@@ -0,0 +1,8 @@
+package com.bsa.spring5entity.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PublisherController {
+
+}
